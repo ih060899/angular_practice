cc86588427538304f971468421b68c70d3b606f6
imran.hossain2189 <imran.hossain2189@bracits.com>
2024-11-12 15:27:05 +0600

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular_practice' title`, () => {
+  it(`should have the 'loginProjectTemp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_practice');
+    expect(app.title).toEqual('loginProjectTemp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular_practice');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, loginProjectTemp');
   });
 });
